e133166ccb5615af38808222e90bfcd1f8edb975
Bosko Stupar <bosko.stupar@gmail.com>
2016-10-04 22:56:38 +0200

    Finished tests

--- server.js
+++ server.js
@@ -1,24 +1,27 @@
-var express = require("express");
+var express = require( "express" );
 var app = express();
 
 var port = process.env.PORT || 8080;
 
+app.get( '/route', function( req, res ) {
+	res.send( app._router.stack );
+} );
 
-app.get("/new/:url/\/b(http|https)//\/\*", function(req, res) {
-    // used to shorten url and return json data
-    var url = req.params;
-    res.status(200).send(url);
-});
+app.get( "/new/:url(*)", function( req, res ) {
+	// used to shorten url and return json data
+	var url = req.params.url;
+	res.status( 200 ).send( url );
+} );
 
-app.get("/:url", function(req, res) {
-    // used to redirect to shortened url
+app.get( "/:url", function( req, res ) {
+	// used to redirect to shortened url
+	res.send( req.params.url );
+} );
 
-});
 
-
-var server = app.listen(port, function() {
-    console.log('app is up');
-});
+var server = app.listen( port, function() {
+	//console.log( 'app is up' );
+} );
 
 module.exports = server;
 
--- test/shortener-test.js
+++ test/shortener-test.js
@@ -1,24 +1,56 @@
-var request = require("supertest");
+var request = require( "supertest" );
 
 
-describe('url shortener', function() {
+describe( 'URL shortener microservices', function() {
     var server;
     beforeEach( function() {
-        delete require.cache[require.resolve('../server')];
-        server = require('../server');
-    });
-    afterEach( function(done) {
-        server.close(done);
-    });
-    
-    it('gets new url', function(done) {
-        request(server)
-            .get('/new/http://some.url')
-            .expect("http://some.url", done);
-    });
-    it('test 200 response', function(done) {
-        request(server)
-            .get('/new/http://some.url')
-            .expect(200, done);
-    });
-});
+        delete require.cache[ require.resolve( '../server' ) ];
+        server = require( '../server' );
+    } );
+    afterEach( function( done ) {
+        server.close( done );
+    } );
+
+    describe( '/new/ route tests', function() {
+        it( 'Valid new link', function( done ) {
+            request( server )
+                .get( '/new/http://www.google.com' )
+                .expect( "http://www.google.com", done );
+        } );
+
+        it( 'test 200 for valid link', function( done ) {
+            request( server )
+                .get( '/new/http://www.google.com' )
+                .expect( 200, done );
+        } );
+
+        it( 'bad url fail', function( done ) {
+            request( server )
+                .get( '/new/google.com' )
+                .expect( {
+                    "error": "Wrong url format. Make sure you have a valid protocol and real site."
+                }, done );
+        } );
+
+        it( 'test 404 for bad link', function( done ) {
+            request( server )
+                .get( '/new/google.com' )
+                .expect( 404, done );
+        } );
+
+    } );
+
+    describe( 'Shortened url route tests', function() {
+        it( 'valid shortened url', function( done ) {
+            request( server )
+                .get( '/1' )
+                .expect( 302, done );
+        } );
+
+        it( 'bad shortened url', function( done ) {
+            request( server )
+                .get( '/000000002' )
+                .expect( 404, done );
+        } );
+    } );
+} );
