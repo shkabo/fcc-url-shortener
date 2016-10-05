1f1d7dc1951483954e5e93dd3adeb0aaecc8eca0
Bosko Stupar <bosko.stupar@gmail.com>
2016-10-05 08:15:32 +0200

    server.js

--- server.js
+++ server.js
@@ -4,30 +4,30 @@ var app = express();
 var port = process.env.PORT || 8080;
 
 app.get( "/new/:url(*)", function( req, res ) {
-	// used to shorten url and return json data
-	var url = req.params.url;
-	// check if we have http/https
-	if ( url.indexOf( 'http://' ) === -1 && url.indexOf( 'https://' ) === -1 ) {
-		res.status( 404 )
-			.send( {
-				"error": "Wrong url format. Make sure you have a valid protocol and real site."
-			} );
+    // used to shorten url and return json data
+    var url = req.params.url;
+    // check if we have http/https
+    if ( url.indexOf( 'http://' ) === -1 && url.indexOf( 'https://' ) === -1 ) {
+        res.status( 404 )
+            .send( {
+                "error": "Wrong url format. Make sure you have a valid protocol and real site."
+            } );
 
-	} else {
+    } else {
 
-		res.status( 200 ).send( url );
-	}
+        res.status( 200 ).send( url );
+    }
 
 } );
 
 app.get( "/:url", function( req, res ) {
-	// used to redirect to shortened url
-	res.send( req.params.url );
+    // used to redirect to shortened url
+    res.send( req.params.url );
 } );
 
 
 var server = app.listen( port, function() {
-	//console.log( 'app is up' );
+    //console.log( 'app is up' );
 } );
 
 module.exports = server;
